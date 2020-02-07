0a029a4209b98ed9877c18244e43c047536ddc03
tomasMizera <tomas.mizera2@gmail.com>
2020-02-07 10:34:08 +0100

    Spatial index creation + nearestNeighbor

--- __init__.py
+++ __init__.py
@@ -10,23 +10,41 @@
 #---------------------------------------------------------------------
 
 from PyQt5.QtWidgets import QAction, QMessageBox
+from qgis.gui import QgsMapToolEmitPoint
+from qgis.core import QgsSpatialIndex
 
 def classFactory(iface):
-    return MinimalPlugin(iface)
+    return PubTool(iface)
 
 
-class MinimalPlugin:
+class PubTool:
     def __init__(self, iface):
         self.iface = iface
 
     def initGui(self):
-        self.action = QAction('Go!', self.iface.mainWindow())
+        self.action = QAction('Pub!', self.iface.mainWindow())
         self.action.triggered.connect(self.run)
         self.iface.addToolBarIcon(self.action)
 
+        self.initialize()
+
+    def initialize(self):
+        # build index
+        self.index = QgsSpatialIndex(self.iface.activeLayer().getFeatures())
+
+        self.pubTool = QgsMapToolEmitPoint(self.iface.mapCanvas())
+        self.pubTool.canvasClicked.connect(self.handle_click)
+
     def unload(self):
         self.iface.removeToolBarIcon(self.action)
-        del self.action
+        del self.action, self.pubTool, self.index
 
     def run(self):
-        QMessageBox.information(None, 'Minimal plugin', 'Do something useful here')
+        self.iface.mapCanvas().setMapTool(self.pubTool)
+
+    def handle_click(self, point):
+        nearest = self.index.nearestNeighbor(point,1)
+        msg = f'You are at x: {point.x()} y: {point.y()}\
+            \nNearest point to you: {nearest}'
+
+        QMessageBox.information(None, 'Pub Tool', msg)
